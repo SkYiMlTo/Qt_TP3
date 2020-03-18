f39fa07a844db71f6543a626d90165c2ea42d0f0
SkYiMlTo <gogo.bourreau@hotmail.fr>
2020-03-18 15:14:32 +0100

    commit

--- fenetre.cpp
+++ fenetre.cpp
@@ -8,9 +8,19 @@
 fenetre::fenetre(){
     setWindowTitle("IHM");
     this->setMinimumSize(200,200);
-    myBar = new QProgressBar(this);
-    myBar->setValue(50);
-    mySlider =new QSlider(Qt::Horizontal,this);
+//    myBar = new QProgressBar(this);
+//    myBar->setValue(50);
+//    mySlider =new QSlider(Qt::Horizontal,this);
+
+    myBar = new QProgressBar();
+    myBar->setRange(0, 100);
+    myBar->setValue(0);
+    myBar->setGeometry(10, 10, 180, 30);
+
+    mySlider = new QSlider(Qt::Horizontal);
+    mySlider->setRange(0, 100);
+    mySlider->setValue(0);
+    mySlider->setGeometry(10, 40, 180, 30);
 
     QVBoxLayout *verticalLayout = new QVBoxLayout();
     verticalLayout->addWidget(myBar);
