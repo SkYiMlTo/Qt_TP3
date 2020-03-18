2d25d01e79b53cf20fff712837a8f3bb5b48f371
SkYiMlTo <gogo.bourreau@hotmail.fr>
2020-03-18 14:10:31 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/TP3Qt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP3Qt.dir/main.cpp.o"
+  "TP3Qt.pdb"
+  "TP3Qt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP3Qt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
